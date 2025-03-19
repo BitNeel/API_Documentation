9fbfd61ec01a4b06d550054960f9071abbfd4c96
BitNeel <poddarneel@gmail.com>
2025-03-19 12:57:51 -0700

    Added Repository classes in infrastructure project

---
+++ library.Infrastructure/BookRepository.cs
@@ -0,0 +1,32 @@
+﻿using library.core;
+using library.data;
+using Microsoft.EntityFrameworkCore;
+
+namespace library.Infrastructure
+{
+    internal class BookRepository : IRepository<Book>
+    {
+        private readonly LibraryDBContext _dbContext;
+
+        public BookRepository(LibraryDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Book?> GetEntityById(long id)
+        {
+            Book? book = await _dbContext.Books.FindAsync(id);
+            return book;
+        }
+
+        public async Task<List<Book>> ListAsync()
+        {
+            return await _dbContext.Books.ToListAsync();
+        }
+
+        public async Task<List<Book>> ListAsync(Func<Book, bool> predicate)
+        {
+            return await _dbContext.Books.Where(predicate).AsQueryable().ToListAsync();
+        }
+    }
+}
---
+++ library.Infrastructure/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using library.core;
+
+namespace library.Infrastructure
+{
+    internal interface IRepository<T> where T: Entity
+    {
+        Task<T> GetEntityById(long id);
+        Task<List<T>> ListAsync();
+        Task<List<T>> ListAsync(Func<T, bool> predicate);
+    }
+}

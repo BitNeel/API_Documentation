c9856a9be2b18856d4cf8577ebbefddaa24e3031
BitNeel <poddarneel@gmail.com>
2025-03-19 23:48:57 -0700

    Added Application layer This layer encapsulates the business logic

---
+++ library.application/BookService.cs
@@ -0,0 +1,42 @@
+﻿using library.core;
+using library.core.Dtos;
+using Microsoft.EntityFrameworkCore;
+namespace library.application
+{
+    public class BookService : IBookService
+    {
+        private readonly IRepository<Book> _repo;
+
+        public BookService(IRepository<Book> repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<List<BookDto>> GetAllBooks()
+        {
+            return await _repo.ListAsync()
+                .Select(b=> new BookDto { Id=b.ID,Title = b.Title, ISBN = b.ISBN})
+                .ToListAsync();
+        }
+
+        public async Task<Book?> GetBookByID(long id)
+        {
+            Book? found_book = await _repo.GetEntityById(id);
+            return found_book;
+        }
+
+        public async Task<List<BookDto>> SearchBookByISBN(string ISBN)
+        {
+            return await _repo.ListAsync(b => b.ISBN == ISBN)
+                .Select(b=> new BookDto { Id=b.ID,ISBN=b.ISBN, Title=b.Title })
+                .ToListAsync();
+        }
+
+        public async Task<List<BookDto>> SearchBookByName(string name)
+        {
+            return await _repo.ListAsync(b => b.Title.Contains(name))
+                .Select(b=> new BookDto { Id=b.ID,ISBN=b.ISBN, Title=b.Title})
+                .ToListAsync();
+        }
+    }
+}
---
+++ library.application/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using library.core;
+using library.core.Dtos;
+namespace library.application
+{
+    public interface IBookService
+    {
+        Task<Book?> GetBookByID(long id);
+        Task<List<BookDto>> SearchBookByName(string name);
+        Task<List<BookDto>> SearchBookByISBN(string ISBN);
+        Task<List<BookDto>> GetAllBooks();
+    }
+}
